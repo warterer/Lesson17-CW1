42b62644029c25f22bc8634807bca4628bc44ce2
areutskyi <artemik.tamik@gmail.com>
2024-01-09 17:52:50 +0200

    defenition methods

--- Lesson17-CW1/Computer.cpp
+++ Lesson17-CW1/Computer.cpp
@@ -1,1 +1,24 @@
 #include "Computer.h"
+
+Computer::Computer(string name, float frequency, int ozu, bool dvd, float cost)
+{
+    this->name = name;
+    this->frequency = frequency;
+    this->ozu = ozu;
+    this->dvd = dvd;
+    this->cost = cost;
+}
+
+int Computer::getOzu() const
+{
+    return ozu;
+}
+
+void Computer::show() const
+{
+    cout << "Name: " << name << endl;
+    cout << "Frequency: " << frequency << "GHz" << endl;
+    cout << "Ozu: " << ozu << "GB" << endl;
+    cout << "DVD: " << dvd << endl;
+    cout << "Cost: " << cost << "UAH" << endl;
+}
